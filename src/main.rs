77c3993cb25bc1b4bfb1b87e1e83bf96a953b00d
Konkitoman <konkitoman@users.noreply.github.com>
2024-04-22 19:25:05 +0300

    Improvements for duration and help command Now duration output now is controled by duration flags, the help-format was improved and help-duration was added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use std::{fmt::Write, str::FromStr};
+use std::{collections::BTreeMap, fmt::Write, str::FromStr};
 
 use chrono::{Offset, TimeZone};
 use clap::{Parser, Subcommand};
 
+/// A simple date and time manipulator
+/// You can get the time, get how much time elapsed sice,
+/// subtract time to get the duration and add/substrat duration from date and time
+///
+/// Most of the work is done by clap and chrono
 #[derive(Parser)]
+#[command(version, author)]
 pub struct TimeMan {
     /// The date format
     #[arg(short = 'f', default_value = "%a, %d %b %Y %T %z")]
@@ -27,6 +33,8 @@ pub enum TimeManCommand {
     Since {
         date: String,
 
+        duration_flags: Option<String>,
+
         #[arg(short)]
         preety: bool,
     },
@@ -36,6 +44,8 @@ pub enum TimeManCommand {
     Sub {
         from_date: String,
         date: String,
+
+        duration_flags: Option<String>,
 
         #[arg(short)]
         preety: bool,
@@ -64,7 +74,10 @@ pub enum TimeManCommand {
         #[arg(short = 'O')]
         offset: Option<String>,
     },
-    FormatHelp,
+    HelpFormat {
+        get_or_search: Option<String>,
+    },
+    HelpDuration,
 }
 
 fn main() {
@@ -99,12 +112,21 @@ fn main() {
             let date = now.format_with_items(format.iter());
             println!("{date}");
         }
-        TimeManCommand::Since { date, preety } => {
+        TimeManCommand::Since {
+            date,
+            preety,
+            duration_flags,
+        } => {
             let date = parse_date(&format, &time_man.format, &date, "date");
             let now = offset.from_utc_datetime(&chrono::Utc::now().naive_utc());
 
             let since = now - date;
-            let buf = timedelta_to_str(since, ISO8601Flags::all());
+            let buf = timedelta_to_str(
+                since,
+                duration_flags
+                    .map(|str| TimedeltaFlags::new(&str))
+                    .unwrap_or(TimedeltaFlags::all()),
+            );
 
             if preety {
                 println!("{}", timedelta_str_to_preety(&buf));
@@ -116,12 +138,18 @@ fn main() {
             from_date,
             date,
             preety,
+            duration_flags,
         } => {
             let from_date = parse_date(&format, &time_man.format, &from_date, "from_date");
             let date = parse_date(&format, &time_man.format, &date, "date");
 
             let res = from_date - date;
-            let buf = timedelta_to_str(res, ISO8601Flags::all());
+            let buf = timedelta_to_str(
+                res,
+                duration_flags
+                    .map(|str| TimedeltaFlags::new(&str))
+                    .unwrap_or(TimedeltaFlags::all()),
+            );
 
             if preety {
                 println!("{}", timedelta_str_to_preety(&buf));
@@ -186,54 +214,177 @@ fn main() {
 
             println!("{}", date.format_with_items(format.iter()));
         }
-        TimeManCommand::FormatHelp => {
+        TimeManCommand::HelpFormat { get_or_search } => {
+            let mut items = BTreeMap::new();
+
+            items.insert(
+                "%A",
+                r#"Full day of the week names.
+
+Prints a full name in the title case, reads either a short or full name in any case."#,
+            );
+
+            items.insert(
+                "%B",
+                r#"Full month names.
+
+Prints a full name in the title case, reads either a short or full name in any case."#,
+            );
+
+            items.insert(
+                "%C",
+                r#"Gregorian year divided by 100. Implies the non-negative year."#,
+            );
+
+            items.insert(
+                "%D",
+                r#"Date in format: 4/22/2024
+
+Same as format: "%m/%d/%Y""#,
+            );
+            items.insert(
+                "%F",
+                r#"Date in format: 2024-4-22
+
+Same as format: "%Y-%m-%d""#,
+            );
+            items.insert("%G", r#"IsoYear 2024 or 100BCE"#);
+            items.insert("%H", r#"Hour 0-24"#);
+            items.insert("%I", r#"Hour 0-12 zero pad like: 06"#);
+            items.insert("%M", r#"Minute 0-59"#);
+            items.insert("%P", r#"pm/am"#);
+            items.insert(
+                "%R",
+                r#"Hour 0-24 and minute like: 17:00
+
+Same as format: "%H:%M""#,
+            );
+            items.insert("%S", r#"Second 0-59 zero pad like: 06"#);
+            items.insert(
+                "%T",
+                r#"Time like: 17:46:05
+
+Same as format: "%H:%M:%S""#,
+            );
+            items.insert("%U", r#"Week of the year like: 16"#);
+            items.insert("%V", r#"ISO Week of the year like: 17"#);
+            items.insert("%Y", r#"Year like: 2024"#);
+            items.insert(
+                "%Z",
+                r#"Time zone name like: +03:00, don't use!
+
+This cannot be parsed use "%:z""#,
+            );
+            items.insert("%a", r#"Short name of the day of the week, is 3 letters"#);
+            items.insert("%b", r#"Short name of month"#);
+            items.insert("%h", r#"Short name of month"#);
+            items.insert("%d", r#"Day of the month zero pad like: 07"#);
+            items.insert("%e", r#"Day of the month space pad like:  7"#);
+            items.insert("%f", r#"Nanoseconds zero pad like: 000000007"#);
+            items.insert("%g", r#"Year mod 100 like: 24"#);
+            items.insert("%j", r#"Day of the year zero pad like: 013"#);
+            items.insert("%k", r#"Hour 24 space pad"#);
+            items.insert("%l", r#"Hour 12 space pad"#);
+            items.insert("%m", r#"Month space pad"#);
+            items.insert("%n", r#"New line like "\n""#);
+            items.insert("%p", r#"AM/PM"#);
+            items.insert("%r", r#"Time like: 07:08:29 PM and fallback to 18:08:29"#);
+            items.insert(
+                "%s",
+                r#"Timestamp
+
+The number of non-leap seconds since the midnight UTC on January 1, 1970.
+For formatting, it assumes UTC upon the absence of time zone offset."#,
+            );
+            items.insert("%t", r#"tab like: \t"#);
+            items.insert(
+                "%u",
+                r#"Day of the week, where Monday = 1 as Sunday = 7 like: 1"#,
+            );
+            items.insert(
+                "%v",
+                r#"Date like: 22-Apr-2024
+
+Same as format: "%d-%b-%Y""#,
+            );
+            items.insert(
+                "%w",
+                r#"Day of the week, where Sunday = 0 and Saturday = 6 like: 1"#,
+            );
+            items.insert("%y", r#"Year mod 100"#);
+            items.insert(
+                "%+",
+                r#"Date and time like: 2024-04-22T18:20:29.306665267+03:00
+
+Is from RFC3339
+Same as format: "%FT%T%.9f%:z""#,
+            );
+            items.insert("%:z", r#"Timezone offset like: +03:00"#);
+            items.insert("%::z", r#"Timezone offset like: +03:00:00"#);
+            items.insert("%:::z", r#"Timezone offset like: +03"#);
+            items.insert("%.3f", r#"Nanoseconds 3 digits like: .467"#);
+            items.insert("%.6f", r#"Nanoseconds 6 digits like: .467312"#);
+            items.insert("%.9f", r#"Nanoseconds 9 digits like: .432467312"#);
+            items.insert("%3f", r#"Nanoseconds 3 digits like: 467"#);
+            items.insert("%6f", r#"Nanoseconds 6 digits like: 467312"#);
+            items.insert("%9f", r#"Nanoseconds 9 digits like: 432467312"#);
+            items.insert("%%", r#"% like: %"#);
+
+            if let Some(get_or_search) = get_or_search {
+                let get_or_search = get_or_search.trim();
+                if let Some(item) = items.get(get_or_search) {
+                    println!("{get_or_search} : {item}");
+                } else {
+                    let items = items
+                        .iter()
+                        .filter(|(_, item)| item.to_lowercase().contains(get_or_search))
+                        .collect::<Vec<_>>();
+                    let pad = items.iter().map(|(k, _)| k.len()).max().unwrap_or(0);
+                    let mut pad_str = String::default();
+                    for (item, description) in items.iter() {
+                        pad_str.clear();
+                        for _ in item.len()..pad {
+                            pad_str.push(' ')
+                        }
+                        println!(
+                            "{item}{pad_str} : {}",
+                            description.lines().next().unwrap_or("")
+                        );
+                    }
+                }
+            } else {
+                let pad = items.keys().map(|k| k.len()).max().unwrap_or(0);
+                let mut pad_str = String::default();
+                for (item, description) in items.iter() {
+                    pad_str.clear();
+                    for _ in item.len()..pad {
+                        pad_str.push(' ')
+                    }
+                    println!(
+                        "{item}{pad_str} : {}",
+                        description.lines().next().unwrap_or("")
+                    );
+                }
+            }
+        }
+        TimeManCommand::HelpDuration => {
             println!(
-                r#"%%    : %
-%a    : weekday name (e.g., Sun)
-%A    : full weekday name (e.g., Sunday)
-%b    : month name (e.g., Jan)
-%B    : full month name (e.g., January)
-%c    : date and time (e.g., Thu Mar  3 23:05:25 2005)
-%C    : first two digits of %Y
-%d    : day of month (e.g., 01)
-%D    : date; same as %m/%d/%y
-%e    : day of month, space padded; same as %_d
-%F    : full date; like %+4Y-%m-%d
-%g    : last two digits of year of ISO week number (see %G)
-%G    : year of ISO week number (see %V); normally useful only with %V
-%h    : same as %b
-%H    : hour (00..23)
-%I    : hour (01..12)
-%j    : day of year (001..366)
-%k    : hour, space padded ( 0..23); same as %_H
-%l    : hour, space padded ( 1..12); same as %_I
-%m    : month (01..12)
-%M    : minute (00..59)
-%n    : a newline
-%N    : nanoseconds
-%p    : either AM or PM; blank if not known
-%P    : like %p, but lower case
-%q    : quarter of year (1..4)
-%r    : 12-clock (e.g., 11:11:04 PM)
-%R    : 24-clock; same as %H:%M
-%s    : seconds
-%S    : seconds mod 60 (00..60)
-%t    : a tab
-%T    : time; same as %H:%M:%S
-%u    : day of week (1..7)
-%U    : week number of year, with Sunday as first day of week (00..53)
-%V    : ISO week number, with Monday as first day of week (01..53)
-%w    : day of week (0..6); 0 is Sunday
-%W    : week number of year, with Monday as first day of week (00..53)
-%x    : locale's date representation (e.g., 12/31/99)
-%X    : locale's time representation (e.g., 23:13:48)
-%y    : last two digits of %Y
-%Y    : year
-%z    : +hhmm numeric time zone (e.g., -0400)
-%:z   : +hh:mm numeric time zone (e.g., -04:00)
-%::z  : +hh:mm:ss numeric time zone (e.g., -04:00:00)
-%:::z : numeric time zone with : to necessary precision (e.g., -04, +05:30)
-%Z    : alphabetic time zone abbreviation (e.g., EDT)"#
+                r#"This is only for the content of the duration
+Valid flags are:
+Y : Year
+M : Month
+D : Day
+h : Hour
+m : minute
+s : second
+n : nanosecond
+
+They are used like:
+"YMDhmsn" this means that everything is included in duration
+"sn" this means only the seconds and nanoseconds are included but everything is stored in seconds and nanoseconds
+
+The recommended duration flags are "sn"
+                "#
             );
         }
     }
@@ -269,9 +420,9 @@ pub fn parse_date(
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub struct ISO8601Flags(u8);
+pub struct TimedeltaFlags(u8);
 
-impl core::ops::BitOr<Self> for ISO8601Flags {
+impl core::ops::BitOr<Self> for TimedeltaFlags {
     type Output = Self;
 
     fn bitor(self, rhs: Self) -> Self::Output {
@@ -279,13 +430,13 @@ impl core::ops::BitOr<Self> for ISO8601Flags {
     }
 }
 
-impl Default for ISO8601Flags {
+impl Default for TimedeltaFlags {
     fn default() -> Self {
         Self::SECOND | Self::NANOS
     }
 }
 
-impl ISO8601Flags {
+impl TimedeltaFlags {
     const YEAR: Self = Self(1 << 0);
     const MONTH: Self = Self(1 << 1);
     const WEEK: Self = Self(1 << 2);
@@ -313,6 +464,23 @@ impl ISO8601Flags {
     pub fn contains(self, rhs: Self) -> bool {
         self.0 & rhs.0 == rhs.0
     }
+
+    pub fn new(str: &str) -> Self {
+        let mut out = Self::empty();
+        for char in str.chars() {
+            match char {
+                'Y' => out = out | Self::YEAR,
+                'M' => out = out | Self::MONTH,
+                'D' => out = out | Self::DAY,
+                'h' => out = out | Self::HOUR,
+                'm' => out = out | Self::MINUTE,
+                's' => out = out | Self::SECOND,
+                'n' => out = out | Self::NANOS,
+                _ => {}
+            }
+        }
+        out
+    }
 }
 
 const YEAR_IN_SECONDS: i64 = 31_536_000;
@@ -322,7 +490,7 @@ const DAY_IN_SECONDS: i64 = 86400;
 const HOUR_IN_SECONDS: i64 = 3600;
 const MINUTE_IN_SECONDS: i64 = 60;
 
-fn timedelta_to_str(timedelta: chrono::TimeDelta, flags: ISO8601Flags) -> String {
+fn timedelta_to_str(timedelta: chrono::TimeDelta, flags: TimedeltaFlags) -> String {
     let mut out = String::default();
     if timedelta.num_seconds().is_negative() {
         out.push('-');
@@ -331,7 +499,7 @@ fn timedelta_to_str(timedelta: chrono::TimeDelta, flags: ISO8601Flags) -> String
 
     let mut seconds = unsafe { (&timedelta as *const _ as *const i64).offset(0).read() }.abs();
 
-    if flags.contains(ISO8601Flags::YEAR) {
+    if flags.contains(TimedeltaFlags::YEAR) {
         let years = seconds / YEAR_IN_SECONDS;
         if years > 0 {
             seconds -= years * YEAR_IN_SECONDS;
@@ -339,7 +507,7 @@ fn timedelta_to_str(timedelta: chrono::TimeDelta, flags: ISO8601Flags) -> String
         }
     }
 
-    if flags.contains(ISO8601Flags::MONTH) {
+    if flags.contains(TimedeltaFlags::MONTH) {
         let months = seconds / MONTH_IN_SECONDS;
         if months > 0 {
             seconds -= months * MONTH_IN_SECONDS;
@@ -347,7 +515,7 @@ fn timedelta_to_str(timedelta: chrono::TimeDelta, flags: ISO8601Flags) -> String
         }
     }
 
-    if flags.contains(ISO8601Flags::WEEK) {
+    if flags.contains(TimedeltaFlags::WEEK) {
         let weeks = seconds / WEAK_IN_SECONDS;
         if weeks > 0 {
             seconds -= weeks * WEAK_IN_SECONDS;
@@ -355,7 +523,7 @@ fn timedelta_to_str(timedelta: chrono::TimeDelta, flags: ISO8601Flags) -> String
         }
     }
 
-    if flags.contains(ISO8601Flags::DAY) {
+    if flags.contains(TimedeltaFlags::DAY) {
         let days = seconds / DAY_IN_SECONDS;
         if days > 0 {
             seconds -= days * DAY_IN_SECONDS;
@@ -363,14 +531,14 @@ fn timedelta_to_str(timedelta: chrono::TimeDelta, flags: ISO8601Flags) -> String
         }
     }
 
-    if flags.contains(ISO8601Flags::HOUR)
-        || flags.contains(ISO8601Flags::MINUTE)
-        || flags.contains(ISO8601Flags::SECOND)
+    if flags.contains(TimedeltaFlags::HOUR)
+        || flags.contains(TimedeltaFlags::MINUTE)
+        || flags.contains(TimedeltaFlags::SECOND)
     {
         out.push('T');
     }
 
-    if flags.contains(ISO8601Flags::HOUR) {
+    if flags.contains(TimedeltaFlags::HOUR) {
         let hours = seconds / HOUR_IN_SECONDS;
         if hours > 0 {
             seconds -= hours * HOUR_IN_SECONDS;
@@ -378,7 +546,7 @@ fn timedelta_to_str(timedelta: chrono::TimeDelta, flags: ISO8601Flags) -> String
         }
     }
 
-    if flags.contains(ISO8601Flags::MINUTE) {
+    if flags.contains(TimedeltaFlags::MINUTE) {
         let minutes = seconds / MINUTE_IN_SECONDS;
         if minutes > 0 {
             seconds -= minutes * MINUTE_IN_SECONDS;
@@ -386,9 +554,9 @@ fn timedelta_to_str(timedelta: chrono::TimeDelta, flags: ISO8601Flags) -> String
         }
     }
 
-    if flags.contains(ISO8601Flags::SECOND) {
+    if flags.contains(TimedeltaFlags::SECOND) {
         let nanos = unsafe { (&timedelta as *const _ as *const i32).offset(2).read() }.abs();
-        if nanos != 0 && flags.contains(ISO8601Flags::NANOS) {
+        if nanos != 0 && flags.contains(TimedeltaFlags::NANOS) {
             out.write_fmt(format_args!("{seconds}.{}S", nanos)).unwrap();
         } else {
             out.write_fmt(format_args!("{seconds}S")).unwrap();
@@ -546,17 +714,17 @@ fn timedelta() {
 
     let time_delta = TimeDelta::new(1, 32).unwrap();
     assert_eq!(
-        timedelta_to_str(time_delta, ISO8601Flags::all()),
+        timedelta_to_str(time_delta, TimedeltaFlags::all()),
         "PT1.32S".to_owned()
     );
     assert_eq!(
         time_delta,
-        timedelta_from_str(&timedelta_to_str(time_delta, ISO8601Flags::all())).unwrap()
+        timedelta_from_str(&timedelta_to_str(time_delta, TimedeltaFlags::all())).unwrap()
     );
 
     let since = Utc::now().naive_utc() - NaiveDateTime::UNIX_EPOCH;
     assert_eq!(
         since,
-        timedelta_from_str(&timedelta_to_str(since, ISO8601Flags::default())).unwrap()
+        timedelta_from_str(&timedelta_to_str(since, TimedeltaFlags::default())).unwrap()
     );
 }
